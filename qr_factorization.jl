034ffc011b2f05a6397346350eb392865eca3e7b
William Simoni <wilsimoni@gmail.com>
2021-02-10 11:12:21 +0100

    modified qr

--- qr_factorization.jl
+++ qr_factorization.jl
@@ -20,19 +20,56 @@ function householder!(v)
     return s
 end
 
-function qr_factorization!(A::Array{Float64,2}, Q::Array{Float64,2}, R::Array{Float64,2}, v::Array{Float64,1})
+function qr_factorization!(A::Array{Float64,2}, Q::Array{Float64,2}, R::Array{Float64,2}, v::Array{Float64,1}, u::Array{Float64,1} )
     (m,n) = size(A)
 
     #copying elements of A in R
     R .= A
+
     
-    @views for j = 1 : n
-        v .= R[:, j]
-        s = householder!(v[j:end])
-        u = v[j:end]
-        R[j:end,j:end] = R[j:end,j:end] - 2u*(u'*R[j:end,j:end])
-        #R[j:end,j:end] = H*R[j:end,j:end]
-        Q[j:end,j] = v[j:end]
+
+    @views for j = 1 : min(m,n)
+            
+            #copying j-th column of R into v
+            for i = 1 : length(v)
+                v[i] = i < j ? 0 : R[i,j]
+            end
+
+            #calculation householder
+            s = norm(v)
+            
+            if s >= 0
+                s = -s
+            end
+        
+            v[j] = v[j] - s
+
+            norm_v = norm(v)
+
+            for i = j : length(v)
+                v[i] = v[i]/norm_v
+            end
+
+            #calculate R
+            # R = R - 2v*(v'*R)
+
+            for i = j : n
+                sum = 0
+                for t = j : n
+                    sum += v[t]*R[t,i]
+                end
+                u[i] = sum
+            end
+
+            for i = j : n
+                for t = j : n
+                    R[t,i] = R[t,i] - 2*v[t]*u[i]
+                end
+            end
+
+            for i = j : n
+                Q[i,j] = v[i]
+            end
     end
 
     return Q,R
