b416c80d3661e390dab40bd49d0d66d3f1a040d6
William Simoni <wilsimoni@gmail.com>
2021-02-10 09:30:44 +0100

    optimized qr_factorization

--- qr_factorization.jl
+++ qr_factorization.jl
@@ -2,37 +2,38 @@
 
 using LinearAlgebra
 
-function householder(x)
-    norm_x = norm(x)
+function householder!(v)
+    s = norm(v)
     #computing v = x - y where y = [||x||; 0 ...]
-    #Note: I used the deep copy
-    v = deepcopy(x)
-    v[1] = v[1] >= 0 ? v[1] + norm_x : v[1] - norm_x
-    #normalizing v
-    u = v/norm(v)
-    #calculating the Householder matrix
-    #H = I - 2*u*u'
-    return u, norm_x
+
+    if s >= 0
+        s = -s
+    end
+
+    v[1] = v[1] - s
+
+    norm_v = norm(v)
+    for i = 1 : length(v)
+        v[i] = v[i]/norm_v
+    end
+    
+    return s
 end
 
-function qr_factorization(A)
+function qr_factorization!(A::Array{Float64,2}, Q::Array{Float64,2}, R::Array{Float64,2}, v::Array{Float64,1})
     (m,n) = size(A)
-    R = deepcopy(A)
-    for j = 1:n
-        u_j,s_j = householder(R[j:end,j])
-        R[j,j] = s_j
-        R[j+1:n,j] = 0
-        R[j:n, j+1:n] = R[j:m, j+1:n] - 2*u*(u_j'*A[j:end,j+1:end])
-        global Q[:, j:end] = Q[:, j:end] - Q[:, j:end]*u_j*2*u_j'
-    end
-    return Q,R
-end
 
-function test(A)
-    @time begin
-        qr(A);
+    #copying elements of A in R
+    R .= A
+    
+    @views for j = 1 : n
+        v .= R[:, j]
+        s = householder!(v[j:end])
+        u = v[j:end]
+        R[j:end,j:end] = R[j:end,j:end] - 2u*(u'*R[j:end,j:end])
+        #R[j:end,j:end] = H*R[j:end,j:end]
+        Q[j:end,j] = v[j:end]
     end
-    @time begin
-        ;
-    end
+
+    return Q,R
 end
