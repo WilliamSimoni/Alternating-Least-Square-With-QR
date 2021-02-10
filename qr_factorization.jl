c3a21f5c59bb09a156d10bff4f14251d69664ac3
William Simoni <wilsimoni@gmail.com>
2021-02-10 11:42:24 +0100

    bug fixed
    if was wrong

--- qr_factorization.jl
+++ qr_factorization.jl
@@ -26,19 +26,19 @@ function qr_factorization!(A::Array{Float64,2}, Q::Array{Float64,2}, R::Array{Fl
     #copying elements of A in R
     R .= A
 
-    
+    k = min(m,n)
 
-    @views for j = 1 : min(m,n)
+    @views for j = 1 : min(m-1,n)
             
             #copying j-th column of R into v
-            for i = 1 : length(v)
+            for i = 1 : k
                 v[i] = i < j ? 0 : R[i,j]
             end
 
             #calculation householder
             s = norm(v)
             
-            if s >= 0
+            if v[j] >= 0
                 s = -s
             end
         
@@ -53,21 +53,21 @@ function qr_factorization!(A::Array{Float64,2}, Q::Array{Float64,2}, R::Array{Fl
             #calculate R
             # R = R - 2v*(v'*R)
 
-            for i = j : n
+            for i = j : k
                 sum = 0
-                for t = j : n
+                for t = j : k
                     sum += v[t]*R[t,i]
                 end
                 u[i] = sum
             end
 
-            for i = j : n
-                for t = j : n
+            for i = j : k
+                for t = j : k
                     R[t,i] = R[t,i] - 2*v[t]*u[i]
                 end
             end
 
-            for i = j : n
+            for i = j : k
                 Q[i,j] = v[i]
             end
     end
